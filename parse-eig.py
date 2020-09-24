5787faa7d635b28e2ac6e0808e998712bb6d3822
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-09-24 08:47:26 +0800

    new file:   parse-eig.py

---
+++ parse-eig.py
@@ -0,0 +1,47 @@
+import numpy as np
+import sys
+import os
+import re
+
+with open('bd.dat') as f:
+    lines = f.readlines()
+
+    header = lines[0].strip()
+    line = header.strip('\n')
+    shape = re.split('[,=/]', line)
+    nbnd = int(shape[1])
+    nks = int(shape[3])
+    eig = np.zeros((nks, nbnd), dtype=np.float32)
+
+
+    npl = 10
+    dividend = nbnd
+    divisor = npl
+    div = nbnd // npl + 2
+
+    for index, value in enumerate(lines[1:]):
+        value = value.strip(' \n')
+        quotient = index // div
+        remainder = index % div
+
+        if remainder == 0:
+            # save kpt info here.
+            pass
+        else:
+            value = re.split('[ ]+', value)
+            a = (remainder - 1) * npl
+            b = a + len(value)
+            # print(index, quotient, remainder, a, b, len(value))
+            eig[quotient][a:b] = value
+
+
+
+
+
+
+
+
+
+
+
+
